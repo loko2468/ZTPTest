cea0648babf86b0f6d3aa1d8c05b1d05338e841f
Ben <lok.benjamin@gmail.com>
2021-07-16 11:30:58 +0100

    admin

--- energy/usage/admin.py
+++ energy/usage/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Rate, Customer, CustomerReading, File
 
-# Register your models here.
+
+class CustomerReadingInline(admin.TabularInline):
+    model = CustomerReading
+    extra = 0
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'meter_number')
+    inlines = [CustomerReadingInline]
+
+
+class RateAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price_gbp_per_kwh')
+    inlines = [CustomerReadingInline]
+
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Rate, RateAdmin)
