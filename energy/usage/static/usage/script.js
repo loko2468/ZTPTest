1caa90fb2c5d520bc0477babf5d7965785eabc54
Ben <lok.benjamin@gmail.com>
2021-07-16 12:28:05 +0100

    Add highlighting same users

--- energy/usage/static/usage/script.js
+++ energy/usage/static/usage/script.js
@@ -1,5 +1,24 @@
 $ = document.querySelector.bind(document);
+$$ = document.querySelectorAll.bind(document);
+
+
+window.addEventListener('load',
+function () {
+    $$('.customer[data-customer_id]').forEach(x=>{
+        x.onmouseover = function(){omoCustomer(x);};
+        x.onmouseleave = function(){omoCustomer(x);};
+    });
+});
+
 
 function showWinners(){
     $('.highest').classList.toggle('hidden');
 }
+
+ function omoCustomer(div){
+    $$('.customer[data-customer_id="'+div.dataset.customer_id+'"]').forEach(x=>{
+        x.classList.toggle('highlight');
+    });
+
+
+ }
